3c2d99caf896ee8bc2281aa383d346178f11223e
Siarhei Busiel <sju.busel@gmail.com>
2020-06-11 12:46:03 +0300

    Add complete Task 04

---
+++ src/main/java/by/epamtc/jwd/busel/assignment02/Task04.java
@@ -0,0 +1,21 @@
+package by.epamtc.jwd.busel.assignment02;
+
+public class Task04 {
+    private static int findOccurrencesNumber(String testLine, char a) {
+        int number = 0;
+        for (int i = 0; i < testLine.length(); i++) {
+            if (a == testLine.charAt(i)) {
+                number++;
+            }
+        }
+
+        return number;
+    }
+
+    public static void main(String[] args) {
+        String testLine = "aaabbbcccdddfffaaabbbc";
+        int numberOfOccurrences = findOccurrencesNumber(testLine, 'c');
+        System.out.printf("A number of character \"%c\" is %d.\n",
+                'c', numberOfOccurrences);
+    }
+}
