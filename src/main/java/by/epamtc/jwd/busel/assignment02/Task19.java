935bb857f0e440721c169d79f3089b2a250fdf80
Siarhei Busiel <sju.busel@gmail.com>
2020-07-15 20:41:15 +0300

    Correct Task 19 of Assignment 02 according to trainer's remarks (get methods, regEx as MAGIC VALUES, etc)

--- src/main/java/by/epamtc/jwd/busel/assignment02/Task19.java
+++ src/main/java/by/epamtc/jwd/busel/assignment02/Task19.java
@@ -1,13 +1,16 @@
 package by.epamtc.jwd.busel.assignment02;
 
 public class Task19 {
+    private static final String NOT_WORD_SYMBOLS = "[^0-9A-Za-zА-Яа-яёЁ-]+";
+
     private static boolean findOutIfPalindrome(String text) {
-        String editedText = text.replaceAll("[^A-Za-zА-Яа-яёЁ]+", "");
+        String editedText = text.replaceAll(NOT_WORD_SYMBOLS, "");
         StringBuilder reverseEditedText = new StringBuilder(editedText).reverse();
         return editedText.equalsIgnoreCase(new String(reverseEditedText));
     }
 
     public static void main(String[] args) {
+        // Латинский язык: Я - сильная мышь
         String text = "Sum summus mus";
         boolean isPalindrome = findOutIfPalindrome(text);
         System.out.print(isPalindrome);
