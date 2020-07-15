c0366202e762e2931a79ce7d3933e8b047d85964
Siarhei Busiel <sju.busel@gmail.com>
2020-07-15 21:34:59 +0300

    Correct Task 22 of Assignment 02 according to trainer's remarks (get methods, regEx as MAGIC VALUES, etc)

--- src/main/java/by/epamtc/jwd/busel/assignment02/Task22.java
+++ src/main/java/by/epamtc/jwd/busel/assignment02/Task22.java
@@ -1,17 +1,16 @@
 package by.epamtc.jwd.busel.assignment02;
 
 public class Task22 {
+    private static final String WORD_SYMBOL = "[A-Za-zА-Яа-яёЁ-]";
+    private static final String WORD_BOUNDARY_REGEX = "\\b";
+
     private static String deleteWordsByTheirLength(String text, int length) {
-        StringBuilder stringBuilder = new StringBuilder();
-        String delimiter = "";
-        String[] words = text.split("[^A-Za-zА-Яа-яёЁ]+");
-        for (String word : words) {
-            if (word.length() != length) {
-                stringBuilder.append(delimiter).append(word);
-                delimiter = ", ";
-            }
-        }
-        return new String(stringBuilder);
+        StringBuilder sb = new StringBuilder();
+        sb.append(WORD_BOUNDARY_REGEX).append(WORD_SYMBOL).append('{')
+                .append(length).append('}').append(WORD_BOUNDARY_REGEX);
+        String regEx = new String(sb);
+        return text.replaceAll(regEx, "");
+
     }
 
     public static void main(String[] args) {
