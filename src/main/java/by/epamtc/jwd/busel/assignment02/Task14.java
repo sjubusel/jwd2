38dbfe7d750bae5ab6b721e08cf07740aa486c62
Siarhei Busiel <sju.busel@gmail.com>
2020-07-15 18:07:38 +0300

    Correct Task 14 of Assignment 02 according to trainer's remarks (get methods, regEx as MAGIC VALUES, etc)

--- src/main/java/by/epamtc/jwd/busel/assignment02/Task14.java
+++ src/main/java/by/epamtc/jwd/busel/assignment02/Task14.java
@@ -1,8 +1,10 @@
 package by.epamtc.jwd.busel.assignment02;
 
 public class Task14 {
-    private static int getShortestWordLength(String text) {
-        String[] words = text.split("[^A-Za-zА-Яа-яёЁ-]+");
+    private static final String NOT_WORD_SYMBOLS = "[^A-Za-zА-Яа-яёЁ-]+";
+
+    private static int findShortestWordLength(String text) {
+        String[] words = text.split(NOT_WORD_SYMBOLS);
         int minLength = words[0].length();
         for (int i = 1; i < words.length; i++) {
             if (words[i].length() < minLength) {
@@ -17,7 +19,7 @@ public class Task14 {
                 "error sit voluptatem accusantium doloremque laudantium, " +
                 "totam rem aperiam eaque ipsa, facere possimus, omnis " +
                 " voluptas assumenda est, omnis dolor repellendus?";
-        int lengthShortestWord = getShortestWordLength(text);
+        int lengthShortestWord = findShortestWordLength(text);
         System.out.println(lengthShortestWord);
     }
 }
