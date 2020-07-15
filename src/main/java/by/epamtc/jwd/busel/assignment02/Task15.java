3ce94874d84c98101e569f1601eb7401e6aee600
Siarhei Busiel <sju.busel@gmail.com>
2020-07-15 18:20:17 +0300

    Correct Task 15 of Assignment 02 according to trainer's remarks (get methods, regEx as MAGIC VALUES, etc)

--- src/main/java/by/epamtc/jwd/busel/assignment02/Task15.java
+++ src/main/java/by/epamtc/jwd/busel/assignment02/Task15.java
@@ -1,11 +1,24 @@
 package by.epamtc.jwd.busel.assignment02;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task15 {
     private static final String NOT_WORD_SYMBOLS = "[^A-Za-zА-Яа-яёЁ-]+";
+    private static final String WORD_PATTERN = "\\b[A-Za-zА-Яа-яёЁ-]+\\b";
 
-    private static int countWordsNumber(String text) {
+    private static int countWordsNumberUsingSplit(String text) {
         String[] words = text.split(NOT_WORD_SYMBOLS);
         return words.length;
+    }
+
+    private static int countWordsNumberUsingMatcher(String text) {
+        Matcher matcher = Pattern.compile(WORD_PATTERN).matcher(text);
+        int counter = 0;
+        while (matcher.find()) {
+            counter++;
+        }
+        return counter;
     }
 
     public static void main(String[] args) {
@@ -13,7 +26,9 @@ public class Task15 {
                 "error sit voluptatem accusantium doloremque laudantium, " +
                 "totam rem aperiam eaque ipsa, facere possimus, omnis " +
                 " voluptas assumenda est, omnis dolor repellendus?";
-        int wordsNumber = countWordsNumber(text);
-        System.out.println(wordsNumber);
+        int wordsNumber1 = countWordsNumberUsingSplit(text);
+        System.out.printf("Words number by Split: %d\n", wordsNumber1);
+        int wordsNumber2 = countWordsNumberUsingMatcher(text);
+        System.out.printf("Words number by Matcher: %d\n", wordsNumber2);
     }
 }
